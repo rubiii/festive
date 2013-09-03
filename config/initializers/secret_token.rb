1f94cf47008cdcad6a07d029b522bd0bc21cceb8
rubiii <me@rubiii.com>
2013-09-03 19:53:06 +0200

    initial rails skeleton

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Festive::Application.config.secret_key_base = '4c1b72a8f8469fb571dfe6651c1ff49ed8af4e7a0b7da9a0f5be6d01d8079db2d2b1dead0ec45b9a8eb45551eb2a3337858280bd3234da4734fb136ffc84daf0'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Festive::Application.config.session_store :cookie_store, key: '_festive_session'
